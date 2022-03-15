16e2821cd4974471cf233e6a03d8f335ee429529
Andrew Marles <6061539+andrewmarles@users.noreply.github.com>
2022-03-15 10:10:35 -0700

    Commit changes for RGB Plugin.

--- gcode.h
+++ gcode.h
@@ -215,6 +215,7 @@ typedef enum {
     LaserPPI_Rate = 127,                //!< 127 - M127
     LaserPPI_PulseLength = 128,         //!< 128 - M128
     OpenPNP_SetAcceleration = 204,      //!< 204 - M204
+    RGB_Inspection_Light = 356, //!< 356 - M356 // ** Collides with Plasma ** On = 1, Off = 2, RGB white LED inspection light in RGB plugin    
     OpenPNP_FinishMoves = 400,          //!< 400 - M400
     OpenPNP_SettingsReset = 502,        //!< 502 - M502
     Trinamic_ModeToggle = 569,          //!< 569 - M122, Marlin format
